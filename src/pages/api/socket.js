8dd9e1df97c34c1cc63ef15beb7b335b8d2a9fb1
Vinicius Goncalves <viniciusfod@gmail.com>
2023-02-14 00:42:32 -0300

    cria chat e funcionalidades no socket

--- src/pages/api/socket.js
+++ src/pages/api/socket.js
@@ -1,17 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const cors = require('cors');
-const io = require('socket.io')(server);
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
 
 const port = 3001;
+const server = http.createServer(app);
 
-app.use(cors());
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on('connection', (socket) => {
   console.log(`User connected with id: ${socket.id}`);
   socket.on('disconnect', () => {
     console.log(`User disconnected with id: ${socket.id}`);
+  });
+  socket.on('join_room', (roomName) => {
+    console.log(`User: ${socket.id} joined room: ${roomName}`);
+    socket.join(roomName);
+  });
+  socket.on('send_message', (data) => {
+    console.log(`User: ${socket.id} sent message: ${data.message} to room: ${data.room}`);
+    socket.to(data.room).emit('receive_message', data);
+  })
+  socket.on('receive_message', (data) => {
+    console.log(`User: ${socket.id} sent message: ${data.message} to room: ${data.room}`);
+    socket.to(data.room).emit('receive_message', data);
   });
 });
 
